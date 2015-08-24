5b07175f961d78dec5e533c26414d42294b16b05
Sean Holcomb <seanholcomb@gmail.com>
2015-08-24 07:01:59 -0700

    Switched to Zbar

--- alexandria/app/src/main/java/it/jaschke/alexandria/ScannerActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/ScannerActivity.java
@@ -3,22 +3,20 @@ package it.jaschke.alexandria;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 
-import me.dm7.barcodescanner.zbar.BarcodeFormat;
-import me.dm7.barcodescanner.zbar.Result;
-import me.dm7.barcodescanner.zbar.ZBarScannerView;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 //Activity from https://github.com/dm77/barcodescanner
-public class ScannerActivity extends Activity implements ZBarScannerView.ResultHandler {
-    private ZBarScannerView mScannerView;
+public class ScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
-        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
         setContentView(mScannerView);                // Set the scanner view as the content view
-
     }
 
     @Override
@@ -33,27 +31,24 @@ public class ScannerActivity extends Activity implements ZBarScannerView.ResultH
         super.onPause();
         mScannerView.stopCamera();           // Stop camera on pause
     }
-
+    
     @Override
     public void handleResult(Result rawResult) {
 
-        if (rawResult.getBarcodeFormat()== BarcodeFormat.ISBN10 || rawResult.getBarcodeFormat()== BarcodeFormat.ISBN10){
-            Log.e("TAG", rawResult.getContents()); // Prints scan results
-            Log.e("TAG", rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
-            String ean =rawResult.getContents();
-            if(ean.length()==10 && !ean.startsWith("978")){
-                ean="978"+ean;
-            }
+        //if (rawResult.getBarcodeFormat()== BarcodeFormat.EAN_13 || rawResult.getBarcodeFormat()== BarcodeFormat.EAN_8){
+            //Log.v(TAG, rawResult.getText()); // Prints scan results
+            //Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+            String ean =rawResult.getText();
+            //if(ean.length()==10 && !ean.startsWith("978")){
+            //    ean="978"+ean;
+            //}
             SharedPreferences.Editor editor=getSharedPreferences("it.jaschke.alexandria",MODE_PRIVATE).edit();
             editor.putString("SCANNER_EAN", ean);
             editor.commit();
 
             finish();
-            //Intent bookIntent = new Intent(getActivity(), BookService.class);
-            //bookIntent.putExtra(BookService.EAN, ean);
-            //bookIntent.setAction(BookService.FETCH_BOOK);
-            //getActivity().startService(bookIntent);
 
-        }
+
+        //}
     }
 }
